5294c5320973b91b55d6f96b830e70d4f0e3a91a
Ahmet Demirci <demirciahmmet2631@gmail.com>
2025-12-11 16:59:43 +0300

    deneme

--- veri_analiz_asistani.c
+++ veri_analiz_asistani.c
@@ -1,18 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h
-#include <stdlib.h>
 
 /*
  *KİŞİSEL VERİ ANALİZİ PROJESİ
  */
 
 int main() {
-    FILE * data
-    data = fopen("data.txt", "r");
-    if (data == NULL) {
-        printf("Error in opening file\n");
-        exit (EXIT_FAILURE);
-    }
+   printf("Kisisel veri analiz asistani basladi");
+    return 0;
 
 
 }
