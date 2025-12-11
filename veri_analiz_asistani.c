0ecbaac0c7ffa001e2400908a7834863c874d2ad
Ahmet Demirci <demirciahmmet2631@gmail.com>
2025-12-11 16:16:26 +0300

    İlk proje dosyaları eklendi

---
+++ veri_analiz_asistani.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h
+#include <stdlib.h>
+
+/*
+ *KİŞİSEL VERİ ANALİZİ PROJESİ
+ */
+
+int main() {
+    FILE * data
+    data = fopen("data.txt", "r");
+    if (data == NULL) {
+        printf("Error in opening file\n");
+        exit (EXIT_FAILURE);
+    }
+
+
+}
